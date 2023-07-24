0b59c8ae58d5643094afac47232ddae6082c8d54
Rico Rose <r.andereck@gmail.com>
2023-07-23 21:06:31 -0500

    test

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,27 @@
+import satData from "./components/satData"
+import {filterByType, setSat, displaySats} from "../App";
+
+
 const Buttons = () => {
+  const filterByType = props.filterByType;
+  const setSat = props.setSat;
+  const displaySats = props.displaySats;
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
-  );
-};
+    {displaySats.map((sat, id) => {
+      return (
+
+        
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      );
+    })}
+
+    <button onClick={() => setSat(satData)}>
+     All Orbits </button>
+  </div>
+)};
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,21 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((sat, index) => (
+             <tr> key={index}>
+         <td>{sat.name}</td>
+          <td>{sat.type}</td>
+          <td>{sat.launchDate}</td>
+          <td>{sat.orbitType}</td>
+          </tr>
+          ))};
         </tbody>
       </table>
   );
